a907dca4956d61c10c1cb2afb6e65fefe2c7f253
1006950402 <1006950402@qq.com>
2017-12-14 17:17:31 +0800

    登录页面

---
+++ TraBal/src/com/trabal/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.trabal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_login);
+	}
+
+}
