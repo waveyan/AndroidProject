9058e23900bf3177787925906d99fd7e17a9a212
1006950402 <1006950402@qq.com>
2017-12-15 14:19:22 +0800

    注册页面

--- TraBal/src/com/trabal/LoginActivity.java
+++ TraBal/src/com/trabal/LoginActivity.java
@@ -1,15 +1,46 @@
 package com.trabal;
 
+import com.trabal.RegisterActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity{
 	
+	
+	private TextView registerTV;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_login);
+		
+		initView();
+		registerTV=(TextView)this.findViewById(R.id.register1ID);
+		
+		registerTV.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+				LoginActivity.this.startActivity(intent);
+				
+			}
+		});
+		
+	
 	}
+	
+	public void initView(){
+		
+		
+		
+	}
+	
+
 
 }
+
