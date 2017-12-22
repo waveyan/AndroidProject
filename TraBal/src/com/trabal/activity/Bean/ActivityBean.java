603beae730d4b7a035251230cde1e1b70500c0b8
waveyan <593310640@qq.com>
2017-12-22 10:03:55 +0800

    .

---
+++ TraBal/src/com/trabal/activity/Bean/ActivityBean.java
@@ -0,0 +1,101 @@
+package com.trabal.activity.Bean;
+
+import com.trabal.hotspot.Bean.HotSpotBean;
+
+public class ActivityBean {
+	private String id;
+	private String title;
+	private String subject;
+	private String type;
+	private String introduction;
+	private String persion;
+	private String telephone;
+	private String website;
+	private String pic1;
+	private String pic2;
+	private String pic3;
+	private String price;
+	private HotSpotBean hsb;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getPersion() {
+		return persion;
+	}
+	public void setPersion(String persion) {
+		this.persion = persion;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getPic1() {
+		return pic1;
+	}
+	public void setPic1(String pic1) {
+		this.pic1 = pic1;
+	}
+	public String getPic2() {
+		return pic2;
+	}
+	public void setPic2(String pic2) {
+		this.pic2 = pic2;
+	}
+	public String getPic3() {
+		return pic3;
+	}
+	public void setPic3(String pic3) {
+		this.pic3 = pic3;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public HotSpotBean getHsb() {
+		return hsb;
+	}
+	public void setHsb(HotSpotBean hsb) {
+		this.hsb = hsb;
+	}
+	
+	
+
+}
