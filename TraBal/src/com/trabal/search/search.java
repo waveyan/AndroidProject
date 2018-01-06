08e2bfd7fb446deb0b532a0ced558032b2eaca24
Wu Tianci <willcream@126.com>
2018-01-06 15:47:48 +0800

    search

--- TraBal/src/com/trabal/search/search.java
+++ TraBal/src/com/trabal/search/search.java
@@ -35,49 +35,6 @@ public class search extends Activity {
 		Intent intent = search.this.getIntent();
 		user = (UserBean) intent.getSerializableExtra("user");
 
-		linears = new ArrayList<LinearLayout>();
-		linears.add(new PositonLinearLayout(search.this, user));
-		linears.add(new UserLinearLayout(search.this, user));
-
-		// 获取内容组件
-		content3 = (android.support.v4.view.ViewPager) this
-				.findViewById(R.id.content3);
-		positionTv = (TextView) this.findViewById(R.id.ptID);
-		userTv = (TextView) this.findViewById(R.id.userID);
-
-		positionTv.setTextColor(android.graphics.Color.CYAN);
-		userTv.setTextColor(android.graphics.Color.BLACK);
-
-		// 设置适配器
-		content3.setAdapter(new CustomPager());
-		// 设置当前页面
-		content3.setCurrentItem(0);
-		// 设置内容组件事件处理
-		content3.setOnPageChangeListener(new CustomPagerChange());
-
-		positionTv.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				content3.setCurrentItem(0);
-				positionTv.setTextColor(android.graphics.Color.CYAN);
-				userTv.setTextColor(android.graphics.Color.BLACK);
-
-			}
-		});
-
-		userTv.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				content3.setCurrentItem(1);
-				positionTv.setTextColor(android.graphics.Color.BLACK);
-				userTv.setTextColor(android.graphics.Color.CYAN);
-
-			}
-		});
 		// 返回上层界面
 		textView = (TextView) findViewById(R.id.cancel);
 		textView.setOnClickListener(new OnClickListener() {
@@ -91,65 +48,5 @@ public class search extends Activity {
 				finish();
 			}
 		});
-	}
-
-	/**
-	 * 内容组件适配器
-	 */
-	private class CustomPager extends PagerAdapter {
-
-		@Override
-		public int getCount() {
-			return linears.size();
-		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return arg0 == arg1;
-		}
-
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			container.removeView(linears.get(position));
-		}
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-
-			container.addView(linears.get(position));
-			return linears.get(position);
-
-		}
-
-	}
-
-	/**
-	 * 
-	 * 内容事件处理
-	 * 
-	 */
-	private class CustomPagerChange implements OnPageChangeListener {
-
-		@Override
-		public void onPageScrollStateChanged(int arg0) {
-		}
-
-		@Override
-		public void onPageScrolled(int arg0, float arg1, int arg2) {
-		}
-
-		@Override
-		public void onPageSelected(int arg0) {
-			if (arg0 == 0) {
-				positionTv.setTextColor(android.graphics.Color.CYAN);
-				userTv.setTextColor(android.graphics.Color.BLACK);
-
-			} else if (arg0 == 1) {
-				positionTv.setTextColor(android.graphics.Color.BLACK);
-				userTv.setTextColor(android.graphics.Color.CYAN);
-
-			}
-		}
-
 	}
 }
